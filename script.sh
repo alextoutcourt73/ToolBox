35a678a59c99b9e91daabebdebe4e90e72a7592c
Alex <axelmertz90@gmail.com>
2025-03-02 23:12:52 +0100

    update

--- script.sh
+++ script.sh
@@ -1,76 +1,66 @@
 #!/usr/bin/bash
 
-# Fonction pour vérifier si un programme est installé
-check_installation() {
-    if type -p $1 &>/dev/null; then
-        echo "$1 est installé !"
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Fonction pour installer un programme
-install_program() {
-    local program=$1
-    echo "$program n'est pas installé, souhaitez-vous l'installer ? [y/n]"
-    read -p "[>]" int
-    if [[ $int == "y" ]]; then
-        apt install -y $program
-        apt upgrade || upgrade
-    else
-        echo "OK"
-        exit 1
-    fi
-}
-
-# Vérification des privilèges root
-if [[ $EUID -ne 0 ]]; then
-   echo "Ce script doit être exécuté en tant que root" 
-   exit 1
-fi
-
 while true; do
-    cat << EOF
+        cat << EOF
 
-         #######                      ######                
-            #     ####   ####  #      #     #  ####  #    # 
-            #    #    # #    # #      #     # #    #  #  #  
-            #    #    # #    # #      ######  #    #   ##   
-            #    #    # #    # #      #     # #    #   ##   
-            #    #    # #    # #      #     # #    #  #  #  
-            #     ####   ####  ###### ######   ####  #    # 
+             #######                      ######                
+                #     ####   ####  #      #     #  ####  #    # 
+                #    #    # #    # #      #     # #    #  #  #  
+                #    #    # #    # #      ######  #    #   ##   
+                #    #    # #    # #      #     # #    #   ##   
+                #    #    # #    # #      #     # #    #  #  #  
+                #     ####   ####  ###### ######   ####  #    # 
 
-        ToolBox facilite l'installation de l'essentiel pour 
-        Transformer votre distribution en serveur.
-        Assurez-vous d'être en root avant d'exécuter le script
+            ToolBox facilite l'installation de l'essentiel pour 
+            Transformer votre distribution en serveur.
+            Assurez-vous d'être en root avant d'exécuter le script
 
-        1. Mettre en place les éléments essentiels (Docker, etc.)
-        2. Mettre en place les éléments essentiels de sécurité
-        3. Faire une backup du pc (a faire avant tout)
-        99. Quit
+            1. Mettre en place les éléments essentiels (Docker, etc.)
+            2. Mettre en place les éléments essentiels de sécurité
+            3. Faire une backup du pc (a faire avant tout)
+            99. Quit
 EOF
 
     read -p "->" REPLY
 
     case $REPLY in
-        1)
-            if ! check_installation docker; then
-                install_program docker
+    1) 
+        if type -p docker &>/dev/null; then
+            echo "Docker est installé !"
+            exit 1
+        fi
+        
+        if ! type -p docker &>/dev/null; then
+            echo "Docker n'est pas installé, souhaitez-vous l'installer ? [y/n]"
+            read -p "[>]" int
+            if (( int == "y" )); then
+                curl https://raw.githubusercontent.com/s4dic/dockerautoinstall/main/dockerautoinstall.sh | bash
+                apt update && apt upgrade
+            else
+                echo "OK"
+                exit 1
             fi
-            ;;
-        2)
-            echo "Mise en place des éléments essentiels de sécurité"
-            if ! check_installation ufw; then
-                install_program ufw
+        fi;;
+
+    2) 
+        echo "Mise en place des éléments essentiels de sécurité"
+
+        if ! type -p ufw &>/dev/null; then
+            echo "Voulez-vous installé UFW ? [y/n]"
+            read -p "[>]" int
+            
+            if (( int == "y" )); then
+                apt install -y ufw
+                
+            else
+                echo "OK"
+                exit 1
             fi
-            ;;
-        99)
-            echo "Arrêt du programme"
-            break
-            ;;
-        *)
-            echo "Option invalide, veuillez réessayer."
-            ;;
+        fi
+        ;;
+        
+    99)
+          echo "Arret du prgramme"
+          break;
     esac
 done
